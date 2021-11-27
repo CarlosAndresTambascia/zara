2e43bc77c29bcec3cdb2fceff22ef893e3263c7e
Carlos Tambascia <carlos.tambascia@gmail.com>
2021-11-27 20:19:43 -0300

    Add missing validation

--- src/main/java/com/between/zara/model/PriceRequest.java
+++ src/main/java/com/between/zara/model/PriceRequest.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class PriceRequest {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd-HH.mm.ss")
+    @NotNull(message = "Application Id must not be null")
     private Date applicationDate;
 
     @PositiveOrZero(message = "Product Id must be positive value")
